e7816ba1e2d83948db339fa9798dc8b8746bdf5a
KingKellee <kellyiyogun@gmail.com>
2022-02-24 23:23:27 +0100

    using jwt for authetication

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error');
+const BadRequest = require('./bad-request');
+const UnautheticatedError = require('./unautheticated');
+
+module.exports = {
+  CustomAPIError,
+  BadRequest,
+  UnautheticatedError,
+};
